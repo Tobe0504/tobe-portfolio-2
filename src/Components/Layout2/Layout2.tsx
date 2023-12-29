23dec24a576733291ef63b47c86db8240cd8fa99
Tobe0504 <ezimorahtobenna@gmail.com>
2023-12-29 09:17:30 +0100

    made work header responsive

--- src/Components/Layout2/Layout2.tsx
+++ src/Components/Layout2/Layout2.tsx
@@ -12,6 +12,23 @@ const Layout2 = ({ children }: Layout2Props) => {
         <span>
           <svg
             xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 25 25"
+            fill="none"
+          >
+            <path
+              d="M6.14126 6.66406L18.3369 18.8597M16.3043 6.66406H6.14126V16.8271"
+              stroke="#0F0C13"
+              strokeWidth="2.74637"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </span>
+      </div>
+      <div className={classes.rightSection}>
+        <span className={classes.back}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
             width="25"
             height="25"
             viewBox="0 0 25 25"
@@ -20,14 +37,14 @@ const Layout2 = ({ children }: Layout2Props) => {
             <path
               d="M6.14126 6.66406L18.3369 18.8597M16.3043 6.66406H6.14126V16.8271"
               stroke="#0F0C13"
-              stroke-width="2.74637"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="2.74637"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             />
           </svg>
         </span>
+        {children}
       </div>
-      <div className={classes.rightSection}>{children}</div>
     </section>
   );
 };
